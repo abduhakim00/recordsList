401535767af118e3a0eb716f1b88832663f8aad0
Hakymichi <awesomeabdu@gmail.com>
2023-01-12 19:18:50 +0500

    Filled in the methods and added some validation for fields

--- grocery.py
+++ grocery.py
@@ -1,19 +1,36 @@
+import main
+
+
 class Grocery:
-    filename = 'groceriesList.txt'
+    filename = main.FILENAME
 
     def delete_record(self, record_name):
-        pass
+        with open(self.filename, 'r') as f:
+            data = f.readlines()
+        data = filter(lambda x: x.split('-')[0] != record_name, data)
+        with open(self.filename, 'w') as w:
+            w.writelines(data)
 
     def edit_record(self, record_name, new_record):
-        pass
+        with open(self.filename, 'r') as f:
+            data = f.readlines()
+        for i in range(len(data)):
+            if data[i].split('-')[0] == record_name:
+                data[i] = new_record+'\n'
+        with open(self.filename, 'w') as w:
+            w.writelines(data)
 
     def add_record(self, record):
-        pass
+        with open(self.filename, 'a') as f:
+            f.write(record+'\n')
 
     @classmethod
     def find_total(cls):
-        pass
+        with open(cls.filename, 'r') as f:
+            data = map(lambda x: int(x.split('-')[-1].split('\\')[0]), f.readlines())
+            print(sum(data))
 
     @classmethod
     def set_filename(cls, text):
-        cls.filename = text;
+        cls.filename = text
+
--- main.py
+++ main.py
@@ -1,57 +1,69 @@
 import sys
 from grocery import Grocery
+FILENAME = 'groceriesList.txt'
 
 
 def start(args=None):
     print(f'Welcome to Your Grocery list')
+    while True:
 
-    if args is not None:
-        Grocery.set_filename(args[1])
-        if args[2] not in ['add', 'delete', 'update', 'sum']:
-            print("Второй аргумент должен быть одним из следующих вариантов: ['add', 'delete', 'update', 'sum'] ")
-            return
-        setAction = args[2]
-    else:
-        print("выберите операцию из списка!")
-        setAction = input('add, delete, update, sum: ')
-        if setAction not in ['add', 'delete', 'update', 'sum']:
-            print("Операция должна быть одна из следующих вариантов: ['add', 'delete', 'update', 'sum'] ")
-            return
+        if args is not None:
+            Grocery.set_filename(args[1])
+            if args[2] not in ['add', 'delete', 'update', 'sum']:
+                print("Второй аргумент должен быть одним из следующих вариантов: ['add', 'delete', 'update', 'sum'] ")
+                return
+            setAction = args[2]
+            args = None
+        else:
+            print('~'*20)
+            print("выберите операцию из списка!")
+            print('Для выхода введите цифру 1!')
+            print('~'*20)
 
-    # инициализация класса
-    groceryList = Grocery()
+            setAction = input('add, delete, update, sum: ')
+            if setAction not in ['add', 'delete', 'update', 'sum', '1']:
+                print("Операция должна быть одной из следующих вариантов: ['add', 'delete', 'update', 'sum'] ")
+                return
 
-    # выбор действия
-    name = input("Пожалуйста введите наименование продукта для совершения операции: ")
-    while not name:
-        name = input("Пожалуйста введите наименование продукта для совершения операции: ")
-    if setAction == 'add':
-        print('Введите все данные')
-        new_data = get_data()
-        groceryList.add_record(new_data)
-    elif setAction == 'sum':
-        Grocery.find_total()
-    elif setAction == 'delete':
-        groceryList.delete_record(name)
-    else:
-        print('Введите новые данные')
-        new_data = get_data()
-        groceryList.edit_record(name, new_data)
+        # инициализация класса
+        groceryList = Grocery()
+
+        # выбор действия
+        if setAction == '1':
+            break
+        elif setAction == 'add':
+            print('Введите все данные')
+            new_data = get_data()
+            groceryList.add_record(new_data)
+        elif setAction == 'sum':
+            Grocery.find_total()
+        elif setAction == 'delete':
+            name = input("Пожалуйста введите наименование продукта для совершения операции: ")
+            while not name:
+                name = input("Пожалуйста введите наименование продукта для совершения операции: ")
+            groceryList.delete_record(name)
+        else:
+            name = input("Пожалуйста введите наименование продукта для совершения операции: ")
+            while not name:
+                name = input("Пожалуйста введите наименование продукта для совершения операции: ")
+            print('Введите новые данные')
+            new_data = get_data()
+            groceryList.edit_record(name, new_data)
 
 
 def get_data():
     while True:
         name = input("Наименование Продукта: ")
         price = input("Цена продукта: ")
-        if name and price:
+        if len(name.split('-')) == 1 and price.isnumeric():
             print(name+'-'+price)
             return name+'-'+price
         else:
-            print("Пожалуйста ответьте на все требования!")
+            print("Пожалуйста убедитесь что цена указана правильно и что в наименовании продукта нет дефизов!")
 
 
 if __name__ == '__main__':
     if len(sys.argv) == 3 or len(sys.argv) == 1:
         start(sys.argv) if len(sys.argv) == 3 else start()
     else:
-        print(f'Мы ожидали либо 2 аргумента, либо без аргументов, но получили {len(sys.argv) - 1} ')
+        print(f'Мы ожидали либо 2 аргумента, либо никаких аргументов, но получили {len(sys.argv) - 1} ')
